0bd4ec09faa8fe9435378aab10f2dac88ceb22ea
Leni Z <magdaz@gmail.com>
2017-04-27 16:22:39 +0200

    Update phon.R

--- phon.R
+++ phon.R
@@ -1,9 +1,9 @@
 phon <- function(pValues,
                      alpha = 0.05,
                      kappa = 0.001){
-  # pool the p-values
+  ## pool the p-values, partial conjunction
   depth <- list.depth(pValues)
-  pconj <- vector("list",length(pValues))
+  pconj <- vector("list",depth)
   pconj.aux <- pValues
   ml <- vector("list", depth)
   for (j in 1:(depth-1)){
@@ -12,7 +12,17 @@ phon <- function(pValues,
   }
   
   ml[[depth]] <- unlist(lapply(pconj.aux,length)) # size of the families
-
-return(list(ml, pconj.aux))
+  stru <- lapply(ml, function(x) rep(1:length(x),x))
+  # obtain u_l for partial conjunction
+  ul <- as.integer(ml[[depth]]*kappa)
+  ul[ul<ml[[depth]]*kappa] <- ul[ul<ml[[depth]]*kappa] + 1
+  # p.pool
+  for (j in 1:length(pconj.aux)){pconj[[depth]] <- p.pool(pconj.aux[[j]], u=ul[j])}
+  for (jj in 1:(depth-1)){
+    for (j in 1:length(ml[[2]])){pconj[[2]] <- c(pconj[[2]], p.pool(pconj[[3]][stru[[2]]==j], u=1) )}
+    for (j in 1:length(ml[[1]])){pconj[[1]] <- c(pconj[[1]], p.pool(pconj[[2]][stru[[1]]==j], u=1) )}
+  }
+  
+  # 
+return(list(ml, pconj.aux,ul,pconj))
 }
-

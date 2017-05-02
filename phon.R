e4d509eda2512fcdd6731791847c97a782d13155
Leni Z <magdaz@gmail.com>
2017-05-02 13:03:30 +0200

    Update phon.R

--- phon.R
+++ phon.R
@@ -16,13 +16,21 @@ phon <- function(pValues,
   # obtain u_l for partial conjunction
   ul <- as.integer(ml[[depth]]*kappa)
   ul[ul<ml[[depth]]*kappa] <- ul[ul<ml[[depth]]*kappa] + 1
-  # p.pool
+  # pool pvalues of families
   for (j in 1:length(pconj.aux)){pconj[[depth]] <- c(pconj[[depth]],p.pool(pconj.aux[[j]], u=ul[j]) )}
+  # pool pooled pvalues
   for (jj in 1:(depth-1)){
     idx <- depth-jj
     for (j in 1:length(ml[[idx]])){pconj[[idx]] <- c(pconj[[idx]], p.pool(pconj[[idx+1]][stru[[idx]]==j], u=1) )}
   }
   
-  # 
-return(list(ml, pconj.aux,ul,pconj))
+  ## screen 
+  screen <- lapply(pconj, function(x) x<=alpha*kappa)
+  idx <- screen[[1]]
+  for (j in 2:depth){
+    idx<-rep(idx,ml[[j-1]])
+    idx<-idx*screen[[j]]
+  }
+  
+return(list(ml, pconj.aux,ul,pconj,screen,idx))
 }

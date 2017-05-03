231021c3b9d352157fef94131ece74bba6ba23c0
Leni Z <magdaz@gmail.com>
2017-05-03 12:35:00 +0200

    Create listdepth.R

---
+++ listdepth.R
@@ -0,0 +1,8 @@
+list.depth <- function(this, thisdepth = 0) {
+  # http://stackoverflow.com/a/13433689/1270695
+  if(!is.list(this)) {
+    return(thisdepth)
+  } else {
+    return(max(unlist(lapply(this, list.depth, thisdepth = thisdepth+1))))    
+  }
+}
